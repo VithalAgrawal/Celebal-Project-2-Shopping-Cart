7fddd4a113fc1097fc0a87dededf55e1eae3beef
Vithal Agrawal <vithalagrawal6@gmail.com>
2023-06-17 23:50:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import Finish from './components/Finish';
 
-function App() {
+
+
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = product => {
+    const existingItem = cartItems.find(item => item.id === product.id);
+    if (existingItem) {
+      const updatedCart = cartItems.map(item => {
+        if (item.id === product.id) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+      });
+      setCartItems(updatedCart);
+    } else {
+      const newCartItem = { ...product, quantity: 1 };
+      setCartItems([...cartItems, newCartItem]);
+    }
+  };
+
+  const updateCart = updatedCartItems => {
+    setCartItems(updatedCartItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <div className='logo'>
+            <ul>
+              <li>
+                <Link to="/">CSI Shop</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Products</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart ({cartItems.length})</Link>
+                {/* <Link to="/cart">Cart</Link> */}
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<Products addToCart={addToCart} />}>
+            {/* <Products addToCart={addToCart} /> */}
+          </Route>
+          <Route path="/cart" element={<Cart cartItems={cartItems} updateCart={updateCart} />}>
+            {/* <Cart cartItems={cartItems} updateCart={updateCart} /> */}
+          </Route>
+          <Route path="/finish" element={<Finish/>}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Finish.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Finish = () => {
+    return (
+        <div>
+            <br/>
+            <br/>
+            <h1>Checkout Succesful!</h1>
+            <br/>
+            <p className='checkout-text'>Thank you for shopping!</p>
+        </div>
+    );
+};
+
+export default Finish;
